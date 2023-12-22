dc8acb545b644fb4e9cfcba61ee558e6b187c633
Sylvia Mills <me@chatalot.xyz>
2023-12-22 18:58:07 +0000

    Add input and cursor display

--- src/tui/draw.rs
+++ src/tui/draw.rs
@@ -1,19 +1,31 @@
-use super::Terminal;
+use super::{input::CommandInputState, Terminal};
 use bevy::prelude::*;
 use ratatui::{
     layout::Rect,
     prelude::*,
-    style::Style,
+    style::Color,
     widgets::{block::Title, Block, Borders, Paragraph},
 };
 
-pub fn render_system(mut terminal: ResMut<Terminal>) {
-    terminal.0.draw(render).expect("Failed to draw terminal");
+pub fn render_system(
+    mut terminal: ResMut<Terminal>,
+    command_input_state: Res<CommandInputState>,
+    time: Res<Time>,
+) {
+    terminal
+        .0
+        .draw(|frame| {
+            render(
+                frame,
+                &command_input_state,
+                time.elapsed_seconds() % 1. >= 0.5,
+            )
+        })
+        .expect("Failed to draw terminal");
 }
 
 // TODO: Check if frame is too small a frame and complain if so
-fn render(frame: &mut Frame, // , input_text: &String
-) {
+fn render(frame: &mut Frame, input_state: &CommandInputState, cursor_visible: bool) {
     let frame_size = frame.size();
 
     // Render Output section
@@ -27,7 +39,27 @@ fn render(frame: &mut Frame, // , input_text: &String
     // Render command input section
     let input_block_rect = Rect::new(0, frame_size.height - 4, frame_size.width, 3);
     frame.render_widget(
-        Block::new().borders(Borders::ALL).title("Input Command"),
+        Paragraph::new(if cursor_visible {
+            if input_state.cursor_pos == input_state.content.chars().count() {
+                Line::from(vec![input_state.content.clone().into(), "_".into()])
+            } else {
+                Line::from(vec![
+                    input_state.content[..input_state.cursor_pos].into(),
+                    input_state
+                        .content
+                        .chars()
+                        .nth(input_state.cursor_pos)
+                        .unwrap()
+                        .to_string()
+                        .bg(Color::White)
+                        .fg(Color::Black),
+                    input_state.content[input_state.cursor_pos + 1..].into(),
+                ])
+            }
+        } else {
+            Line::from(input_state.content.clone())
+        })
+        .block(Block::new().borders(Borders::ALL).title("Input Command")),
         input_block_rect,
     );
 
