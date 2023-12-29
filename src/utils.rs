d1d75f249b849dfc49771545da7cdd8dccfe7e42
Sylvia Mills <me@chatalot.xyz>
2023-12-29 21:58:20 +0000

    Improve log file title formatting

--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,15 @@
-use std::time::UNIX_EPOCH;
 use tracing::level_filters::LevelFilter;
 use tracing_appender;
 use tracing_subscriber::{self, EnvFilter};
 
 pub fn setup_global_tracing_subscriber(default_log_level: LevelFilter) {
     tracing_subscriber::fmt()
-        .with_writer(tracing_appender::rolling::daily(
+        .with_writer(tracing_appender::rolling::never(
             "logs",
-            "orvin.log", // format!(
-                         //     "{:#?}.log",
-                         //     std::time::SystemTime::now()
-                         //         .duration_since(UNIX_EPOCH)
-                         //         .expect("System time should not be before the Unix Epoch")
-                         // ),
+            format!(
+                "{}",
+                chrono::offset::Utc::now().format("%d-%m-%Y_%H-%M-%S.log")
+            ),
         ))
         .with_env_filter(
             EnvFilter::builder()
