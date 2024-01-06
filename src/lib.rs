3c13b29cbb908d83039c1960b4f927871719d86b
Sylvia Mills <me@chatalot.xyz>
2024-01-06 15:55:32 +0000

    Remove lightyear and `crate::net` for now
    We are considering migrating to `bevy_replicon` instead and `crate::net` was not doing anything yet.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
 pub mod commands;
-pub mod net;
 pub mod tui;
 pub mod utils;
--- src/net/messages.rs
+++
@@ -1,5 +0,0 @@
-use super::OrvinProtocol;
-use lightyear::prelude::*;
-
-#[message_protocol(protocol = "OrvinProtocol")]
-pub enum Messages {}
